b3474fbf33114f2abe3fe289234629f0a80cd61a
Kim Diep <kim.diep.techno@gmail.com>
2021-09-22 15:11:33 +0100

    Adds API Interactive Lab 1 Demo Application

---
+++ src/main/java/com/techreturners/apilab1/controller/CoffeeController.java
@@ -0,0 +1,25 @@
+package com.techreturners.apilab1.controller;
+
+import com.techreturners.apilab1.model.Coffee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class CoffeeController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = "/coffeelover")
+    public String coffeeLover(){
+        return "I like coffee!";
+    }
+
+    @GetMapping("/coffee")
+    public Coffee coffee(@RequestParam(value = "name", defaultValue = "latte") String name) {
+        return new Coffee(counter.incrementAndGet(), name);
+    }
+
+}
